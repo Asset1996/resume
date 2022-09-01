3f107b5a01214b9b94ff6b6436d516e37778f620
Asset1996 <khaitbay96@gmail.com>
2022-09-01 17:55:47 +0500

    Bug fixed

--- app/asgi.py
+++ app/asgi.py
@@ -1,13 +1,5 @@
-"""
-ASGI config for app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -15,6 +7,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
